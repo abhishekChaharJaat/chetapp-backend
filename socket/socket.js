c050487fb5a3b0d904a2470e71e582c680e0ac0c
email <abhishekchahar200@gmail.com>
2024-12-28 14:52:37 +0530

    5173

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
   },
 });
@@ -20,7 +20,7 @@ const userSocketMap = {}; // {userId->socketId}
 
 io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
-  console.log("connected : " + userId); 
+  console.log("connected : " + userId);
   if (userId !== undefined) {
     userSocketMap[userId] = socket.id;
   }
